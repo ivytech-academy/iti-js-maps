59e0a80e554244d3c656633c2808e3c009b45888
ivytech-academy <ivytech524@gmail.com>
2026-05-18 05:24:22 +0300

    Create Lab 3
    Apply renderers to layers

---
+++ web_2_display_layers/starter/main.js
@@ -0,0 +1,27 @@
+//TODO002: Import required modules
+
+
+//TODO003: Import our custom configurations from config.js 
+
+//TODO004: Set API Ket
+
+
+const viewElement = document.querySelector("arcgis-map");
+await viewElement.componentOnReady();
+
+
+//TODO005: Get reference to Map 
+
+
+//TODO006: Create and Add feature layer to the map
+
+
+//TODO008: Wait for the layer to be loaded
+//await reactiveUtils.whenOnce(() => metroStopsLayer.loaded)
+
+
+//TODO009: Wait for the LAYER VIEW to be loaded
+
+
+//TODO010: Create and add routes feature layer to the map
+
